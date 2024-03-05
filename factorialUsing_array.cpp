6dbe6353d61865f6bde188aaa12a0605497f795a
Sachinandan <sachinandanp5@gmail.com>
2024-03-06 01:28:35 +0530

    factorial of large number using array

---
+++ factorialUsing_array.cpp
@@ -0,0 +1,35 @@
+// FACTORIAL OF A LARGE NUMBER USING ARRAY
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n = 945;
+    vector<int> ans;
+    ans.push_back(1);
+    int carry = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        for (int j = 0; j < ans.size(); j++)
+        {
+            int x = ans[j] * i + carry;
+            ans[j] = x % 10;
+            carry = x / 10;
+        }
+        while (carry)
+        {
+            ans.push_back(carry % 10);
+            carry = carry / 10;
+        }
+    }
+    reverse(ans.begin(), ans.end());
+
+    for (int i : ans)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
