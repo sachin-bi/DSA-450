c7b61221143f22c466f87ad3b3fa701a4573fa64
Sachinandan <sachinandanp5@gmail.com>
2024-03-12 12:06:03 +0530

    firstOccurence_usingBinarySearch

--- binarySearch_firstOccurence.cpp
+++ binarySearch_firstOccurence.cpp
@@ -9,7 +9,7 @@ int first_occur(vector<int> arr, int target)
     int e = arr.size() - 1;
     int idx = -1;
     int s = 0;
-    int mid = s + (e-s)/2;
+    int mid = s + (e - s) / 2;
     while (s <= e)
     {
         if (target == arr[mid])
@@ -25,8 +25,9 @@ int first_occur(vector<int> arr, int target)
         {                // cond{tar<arr[mid]}
             e = mid - 1; // search left
         }
-        mid = s + (e-s)/2;
-    }
+        mid = s + (e - s) / 2;
+        }
+    return idx;
 }
 
 int main()
@@ -35,7 +36,7 @@ int main()
     // int arr[]={1,3,4,4,4,4,4,6,7,9};
     // int e = sizeof(arr)/sizeof(int) - 1;
     vector<int> arr{1, 3, 4, 4, 4, 4, 4, 6, 7, 9};
-    int target = 4;
+    int target = 95;
     int occur_idx = first_occur(arr, target);
 
     if (occur_idx == -1)
@@ -44,9 +45,9 @@ int main()
     }
     else
     {
-        cout <<endl<< occur_idx << " -- idx is first occurence of " << target << endl;
+        cout << endl
+             << occur_idx << " -- idx is first occurence of " << target << endl;
     }
-
 
     return 0;
 }
