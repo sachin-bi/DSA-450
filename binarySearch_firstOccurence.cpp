3faa3baf0e65b88d0a1d75307023510b0b322ab9
Sachinandan <sachinandanp5@gmail.com>
2024-03-12 12:00:53 +0530

    firstOccurence_usingBinarySearch

---
+++ binarySearch_firstOccurence.cpp
@@ -0,0 +1,52 @@
+// Find the first occurence of element in arrray..
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int first_occur(vector<int> arr, int target)
+{
+    int e = arr.size() - 1;
+    int idx = -1;
+    int s = 0;
+    int mid = s + (e-s)/2;
+    while (s <= e)
+    {
+        if (target == arr[mid])
+        {
+            idx = mid;
+            e = mid - 1; // search left
+        }
+        else if (target > arr[mid])
+        {
+            s = mid + 1; // search right
+        }
+        else
+        {                // cond{tar<arr[mid]}
+            e = mid - 1; // search left
+        }
+        mid = s + (e-s)/2;
+    }
+}
+
+int main()
+{
+
+    // int arr[]={1,3,4,4,4,4,4,6,7,9};
+    // int e = sizeof(arr)/sizeof(int) - 1;
+    vector<int> arr{1, 3, 4, 4, 4, 4, 4, 6, 7, 9};
+    int target = 4;
+    int occur_idx = first_occur(arr, target);
+
+    if (occur_idx == -1)
+    {
+        cout << target << " not found";
+    }
+    else
+    {
+        cout <<endl<< occur_idx << " -- idx is first occurence of " << target << endl;
+    }
+
+
+    return 0;
+}
